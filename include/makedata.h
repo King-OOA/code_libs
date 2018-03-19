b1874a21ba84bd3f229263e310855e82a9e6bf66
pz <284270220@qq.com>
2018-03-19 23:30:58 +0800

    修改makedata

--- include/makedata.h
+++ include/makedata.h
@@ -1,19 +1,21 @@
 #pragma once
 
-/* #include <stdio.h> */
-/* #include <stdlib.h> */
-/* #include <string.h> */
-/* #include <math.h> */
-/* #include "patset.h" */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "strlist.h"
 
 
 
-/* typedef char Char_T; */
+typedef char Char_T;
 
-/* void make_rand_text(const char *filename, int64_t text_len, Char_T *ch_array,  int32_t ch_num); */
-/* void extract_pats(char const *text_file_name, char const *pats_file_name, */
-/* 		  Pat_Num_T pat_num, Pat_Len_T min_pat_len, Pat_Len_T max_pat_len, */
-/* 		  char const *forbid_ch); */
-/* void file_filter(char const *file_name, int32_t (*filter)(int32_t ch)); */
-/* void make_pats_file(void); */
-/* void make_DNA(int32_t num, int32_t len, char const *file_name); */
+void make_rand_str(Char_T *alphabet, int32_t size, int64_t str_len, const char *ofile);
+
+void extract_strs(char const *ifile, char const *ofile,
+		  Str_Num_T str_num, Str_Len_T min_len, Str_Len_T max_len,
+		  char const *forbids);
+
+void file_filter(char const *file_name, int32_t (*filter)(int32_t ch));
+void make_pats_file(void);
+void make_DNA(int32_t num, int32_t len, char const *file_name);
--- src/makedata.c
+++ src/makedata.c
@@ -9,37 +9,37 @@
 #include "mem.h"
 #include "common.h"
 #include "makedata.h"
-#include "strlist.h"
 
 static char printed_ch[] = {'a','b','c','d','e','f','g','h','i',
-		     'j','k','l','m','n','o','p','q','r',
-		     's','t','u','v','w','x','y','z',
-		     'A', 'B','C','D','E','F','G','H','I','J',
-		     'K','L','M','N','O','P','Q','R','S',
-		     'T','U','V','W','X','Y','Z',
-		     '0','1','2','3','4','5','6','7','8','9',
-		     '~', '`','!','@','#','$','%','^','&','*','(',')',
-		     '-','_','=','+','{','}','[',']','\\','|',
-		     ';',':','\'','"',',','.','<','>','/','?',' ',
-		     '\n','\t'};
+			    'j','k','l','m','n','o','p','q','r',
+			    's','t','u','v','w','x','y','z',
+			    'A', 'B','C','D','E','F','G','H','I','J',
+			    'K','L','M','N','O','P','Q','R','S',
+			    'T','U','V','W','X','Y','Z',
+			    '0','1','2','3','4','5','6','7','8','9',
+			    '~', '`','!','@','#','$','%','^','&','*','(',')',
+			    '-','_','=','+','{','}','[',']','\\','|',
+			    ';',':','\'','"',',','.','<','>','/','?',' ',
+			    '\n','\t'};
 
 static char DNA_ch[] = {'A','C','T','G'};
 
-/* 从长为ch_num的ch_array中选择字符，产生长为text_len的文本 */
-void make_rand_text(const char *filename, int64_t text_len, Char_T *ch_array,  int32_t ch_num)
+/* 从长为size的数组alphabet中随机选择字符，产生长为str_len的文本, 存入文件file中 */
+void make_rand_str(Char_T *alphabet, int32_t size, int64_t str_len, const char *ofile)
 {
-     assert(text_len > 0);
-     assert(ch_array);
-     assert(ch_num > 0);
+    assert(ofile);
+    assert(str_len > 0);
+    assert(alphabet);
+    assert(size > 0);
      
-     FILE *text_fp = efopen(filename, "w");
+    FILE *fp = efopen(ofile, "w");
 	     
-     srand(time(NULL));
+    srand(time(NULL));
      
-     while (text_len--)
-       putc(ch_array[rand_range(0, ch_num-1)], text_fp);
+    while (str_len--)
+	putc(alphabet[rand_range(0, size-1)], fp);
 	
-     efclose(text_fp);
+    efclose(fp);
 }
 
 #define ACMa 16807
@@ -66,91 +66,89 @@ static int aleat(int top)
     return ((double) Seed) * top / ACMm;
 }
 
-/* 从文件file中, 抽取pat_num个模式串, 构成模式串文件pat_file,
-   串长分布:min_pat_len~max_pat_len, 禁止的字符在保存在forbid_ch中 */
-void extract_pats(char const *text_file_name, char const *pats_file_name,
-		  Pat_Num_T pat_num, Pat_Len_T min_pat_len, Pat_Len_T max_pat_len,
-		  char const *forbid_ch)
+/* 从文件ifile中, 抽取str_num个串, 存入文件ofile中, 每行一个.
+   串长分布:min_len~max_len, 禁止的字符在保存在forbids中 */
+void extract_strs(char const *ifile, char const *ofile,
+		  Str_Num_T str_num, Str_Len_T min_len, Str_Len_T max_len,
+		  char const *forbids)
 {
-    size_t text_len;
-    char *text_buf= load_file(text_file_name, &text_len);
+    size_t file_len;
+    char *buf = load_file(ifile, &file_len);
 
-    FILE *pats_fp = efopen(pats_file_name, "w");
+    FILE *ofp = efopen(ofile, "w");
 
-    while (pat_num--) {
-        Pat_Len_T pat_len = rand_range(min_pat_len, max_pat_len); /* 随机产生模式串长 */
-	Pat_Len_T i;
-	size_t pat_pos; /* 每个模式串在文本中的起始位置 */
-        /*对每一个产生的起始位置,检查该位置的pat是否包含禁止字符,如果包含,
-	  则重新产生一个起始位置,直到该位置的pat不包含任何禁止字符 */
+    while (str_num--) {
+        Str_Len_T str_len = rand_range(min_len, max_len); /* 随机产生串长 */
+	Str_Len_T i;
+	size_t pos; /* 在位置pos抽取串 */
+        /* 对每一个产生的pos, 检查该位置的str是否包含禁止字符, 如果包含,
+	  则重新产生一个pos, 直到该位置的str不包含任何禁止字符 */
 	do { 
-             pat_pos = aleat(text_len - pat_len + 1);
-            for (i = 0; i < pat_len; i++)
-                if (strchr(forbid_ch, text_buf[pat_pos+i])) break;
-        } while (i < pat_len);
+	    pos = aleat(file_len - str_len + 1);
+            for (i = 0; i < str_len; i++)
+                if (strchr(forbids, buf[pos+i])) break;
+        } while (i < str_len);
 	
-        for (Pat_Len_T i = 0; i < pat_len; i++)
-            putc(text_buf[pat_pos+i], pats_fp);
+        for (Str_Len_T i = 0; i < str_len; i++)
+            putc(buf[pos+i], ofp);
         
-        putc('\n', pats_fp);      /* 模式串以行为单位 */
+        putc('\n', ofp);      /* 模式串以行为单位 */
     }
 
-    efclose(pats_fp);
+    efclose(ofp);
 	  
-    printf("File %s successfully generated\n", pats_file_name);
+    printf("File %s successfully generated\n", ofile);
 
-    FREE(text_buf);
+    FREE(buf);
 }
 
 void file_filter(char const *file_name, int32_t (*filter)(int32_t ch))
 {
-  FILE *ifile_fp = efopen(file_name, "rb+");
-  char const *ofile_name = str_cat(file_name, "_filtered");
-  FILE *ofile_fp = efopen(ofile_name, "w");
-  FREE(ofile_name);
+    FILE *ifile_fp = efopen(file_name, "rb+");
+    char const *ofile_name = str_cat(file_name, "_filtered");
+    FILE *ofile_fp = efopen(ofile_name, "w");
+    FREE(ofile_name);
 
-  int32_t ch;
-  uint64_t n = 0; /* 被过滤掉的字符数 */
-  printf("\nFiltering: %s\n", file_name);
+    int32_t ch;
+    uint64_t n = 0; /* 被过滤掉的字符数 */
+    printf("\nFiltering: %s\n", file_name);
 
-  while ((ch = getc(ifile_fp)) != EOF)
-    if(filter(ch))
-      putc(ch, ofile_fp);
-    else
-      n++;
+    while ((ch = getc(ifile_fp)) != EOF)
+	if(filter(ch))
+	    putc(ch, ofile_fp);
+	else
+	    n++;
 
-  
-  
- printf("Dnoe! %ld characters filtered!\n", n);
-  efclose(ofile_fp);
-  efclose(ifile_fp);
+    printf("Dnoe! %ld characters filtered!\n", n);
+    efclose(ofile_fp);
+    efclose(ifile_fp);
 }
 
-void make_pats_file(void)
+void make_strs_file(void)
 {
-  char forbid_ch[] = {'\n', '\b', '\0'};
+    char forbid_ch[] = {'\n', '\b', '\0'};
 
-  uint64_t pats_num[] = {100000, 200000, 300000, 400000,
-			 500000, 600000, 700000,
-			 800000, 900000, 1000000,
-			 2000000, 3000000, 4000000,
-			 5000000, 6000000, 7000000,
-			 8000000, 9000000, 10000000,
-			 20000000, 30000000};
+    uint64_t strs_num[] = {100000, 200000, 300000, 400000,
+			   500000, 600000, 700000,
+			   800000, 900000, 1000000,
+			   2000000, 3000000, 4000000,
+			   5000000, 6000000, 7000000,
+			   8000000, 9000000, 10000000,
+			   20000000, 30000000};
   
-  char *pats_file_suffix[] = {"10w", "20w", "30w", "40w", "50w", "60w",
-			   "70w", "80w", "90w", "100w", "200w", "300w",
-			   "400w", "500w", "600w", "700w", "800w", "900w",
-			   "1000w", "2000w", "3000w"};
+    char *strs_file_suffix[] = {"10w", "20w", "30w", "40w", "50w", "60w",
+				"70w", "80w", "90w", "100w", "200w", "300w",
+				"400w", "500w", "600w", "700w", "800w", "900w",
+				"1000w", "2000w", "3000w"};
 
-  int pats_file_num = sizeof(pats_num) / sizeof(*pats_num);
+    int strs_file_num = sizeof(strs_num) / sizeof(*strs_num);
 
-  for (int i = 0; i < pats_file_num; i++) {
-    char pats_file_path[200] = "/home/pz/data/";
-    extract_pats("/home/pz/data/Random/patterns/Random_100M",
-		 strcat(pats_file_path, pats_file_suffix[i]),
-		 pats_num[i], MIN_PAT_LEN, MAX_PAT_LEN, forbid_ch);
-  }
+    for (int i = 0; i < strs_file_num; i++) {
+	char strs_file_strh[200] = "/home/pz/data/";
+	extract_strs("/home/pz/data/Random/strterns/Random_100M",
+		     strcat(strs_file_strh, strs_file_suffix[i]),
+		     strs_num[i], MIN_STR_LEN, MAX_STR_LEN, forbid_ch);
+    }
 }
 
 /* 产生num个长为len的DNA序列, 保存在文件file_name中 */
